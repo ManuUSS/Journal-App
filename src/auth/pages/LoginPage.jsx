ea7bae3f6ec825d48d2d9ab8528bc6e2785c2e1b
Manuel Ulate <fr3zs09@gmail.com>
2023-01-13 14:45:20 -0600

    creacion del layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,36 +1,17 @@
 import { Link as LinkRouter } from 'react-router-dom';
 import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid 
-      container
-      spacing={ 0 }
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}  
-    >
-      <Grid item
-        className="box-shadow"
-        xs={ 3 }
-        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
-      >
-        <Typography
-          variant="h5"
-          sx={{ mb: 1 }}
-        >
-          Login
-        </Typography>
-
-        <form action="">
+    <AuthLayout title='Login'>
+      <form action="">
           <Grid container>
             
             <Grid item 
               xs={ 12 } 
-              sx={{ mt: 2 }}
-            >
+              sx={{ mt: 2 }}>
               <TextField
                 label="Correo"
                 type="email"
@@ -95,8 +76,7 @@ export const LoginPage = () => {
 
           </Grid>
         </form>
+    </AuthLayout>
 
-      </Grid>
-    </Grid>
   )
 }
