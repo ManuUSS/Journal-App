7623abf7fb5686f6831e29389c3ae867231f2e94
Manuel Ulate <fr3zs09@gmail.com>
2023-01-13 13:43:45 -0600

    tema implementado

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { mainTheme } from './';
 
 
 export const AppTheme = ({ children }) => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={ mainTheme }>
         <CssBaseline />
         { children }
     </ThemeProvider>
