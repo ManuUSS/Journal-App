4253fe62df94fbdccd892866cbceb94e00819a23
Manuel Ulate <fr3zs09@gmail.com>
2023-01-14 10:58:45 -0600

    Boton flotante y galeria de imagenes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
@@ -6,8 +7,21 @@ import { NothingSelectedView } from "../views/NothingSelectedView"
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView /> 
+      {/* <NoteView /> */}
+      <IconButton
+        size="small"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: .9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30}} />
+      </IconButton>
     </JournalLayout>
   )
 }
