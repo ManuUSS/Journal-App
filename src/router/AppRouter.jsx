111951fe65951ed3b1d7d678ecb35cda8bb9837e
Manuel Ulate <fr3zs09@gmail.com>
2023-01-15 20:27:18 -0600

    custom hook para manejar la autentificacion

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,13 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { Navigate, Route, Routes } from "react-router-dom"
-import { onAuthStateChanged } from "firebase/auth"
-import { firebaseAuth } from "../firebase/config"
-import { AuthRoutes } from "../auth/routes/AuthRoutes"
-import { JournalRoutes } from "../journal/routes/JournalRoutes"
-import { CheckingAuth } from "../ui/components/CheckingAuth"
-import { login, logout } from "../store/auth"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
 
-  const { status } = useSelector( ( state ) => state.auth );
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    
-    onAuthStateChanged( firebaseAuth, async( user ) => {
-      if( !user ) return dispatch( logout() );
-      const { uid, email, displayName, photoURL } = user;
-      dispatch( login({ uid, email, displayName, photoURL }) );
-    });
-
-  }, []);
+  const { status } = useCheckAuth();
   
-
   if( status === 'checking' ) {
     return <CheckingAuth />
   }
