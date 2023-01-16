eb129cfbbd8bc2272b29c87421cf7ab6ea377a4f
Manuel Ulate <fr3zs09@gmail.com>
2023-01-15 20:21:51 -0600

    mantener estado de autentificacion al recargar

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -14,8 +14,8 @@ export const LoginPage = () => {
   const dispatch = useDispatch();
 
   const { email, password, onInputChange } = useForm({
-    email: 'manuel@google.com',
-    password: '123456'
+    email: '',
+    password: ''
   });
 
   const isAuthenticating = useMemo( () => status === 'checking', [ status ]);
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,28 @@
-import { useSelector } from "react-redux"
-import { Route, Routes } from "react-router-dom"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
+import { firebaseAuth } from "../firebase/config"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
 import { CheckingAuth } from "../ui/components/CheckingAuth"
+import { login, logout } from "../store/auth"
 
 export const AppRouter = () => {
 
   const { status } = useSelector( ( state ) => state.auth );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    
+    onAuthStateChanged( firebaseAuth, async( user ) => {
+      if( !user ) return dispatch( logout() );
+      const { uid, email, displayName, photoURL } = user;
+      dispatch( login({ uid, email, displayName, photoURL }) );
+    });
+
+  }, []);
+  
 
   if( status === 'checking' ) {
     return <CheckingAuth />
@@ -14,8 +30,13 @@ export const AppRouter = () => {
 
   return (
     <Routes>
-        <Route path="auth/*" element={ <AuthRoutes /> } />
-        <Route path="/*" element={ <JournalRoutes /> } />
+      { 
+        ( status === 'authenticated' )
+        ? <Route path="/*" element={ <JournalRoutes /> } />
+        : <Route path="auth/*" element={ <AuthRoutes /> } />
+      }
+
+      <Route path="/*" element={ <Navigate to='/auth/login' /> } />
     </Routes>
   )
 }
