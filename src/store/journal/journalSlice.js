fd649d2c86ad089c7b78396d90c602766ef515c6
Manuel Ulate <fr3zs09@gmail.com>
2023-01-16 13:36:03 -0600

    journal slice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,39 @@
+
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: true,
+        savedMessage: '',
+        notes: [],
+        activeNote: null
+    },
+    reducers: {
+        addNewNote: ( state, action ) => {
+
+        },
+        setActiveNote: ( state, action ) => {
+
+        },
+        setNotes: ( state, action ) => {
+
+        },
+        setSaving: ( state ) => {
+
+        },
+        updateNote: ( state, action ) => {
+
+        },
+        deleteNoteById: ( state, action ) => {
+
+        }
+    }
+});
+
+
+// Action creators are generated for each case reducer function
+export const { 
+            addNewNote, setActiveNote, setNotes, setSaving, updateNote, deleteNoteById 
+        } = journalSlice.actions;
